d34974edf09d2abba82a023024d8e8d640055424
Rick Chen <rick@xempireinc.com>
2021-05-18 07:22:25 -0700

    Add Identify to the DemoController.

--- src/demo/java/com/amplitude/DemoController.java
+++ src/demo/java/com/amplitude/DemoController.java
@@ -1,5 +1,6 @@
 package com.amplitude;
 
+import org.json.JSONObject;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.bind.annotation.RequestMapping;
 
@@ -12,6 +13,9 @@ public class DemoController {
 		Amplitude amplitude = Amplitude.getInstance("INSTANCE_NAME");
 		amplitude.init("8e07b9d451a7d07bd33f6e9ba5870f21");
 		amplitude.logEvent(new Event("Test Event", "test_user_id"));
+		final Identification identification = new Identification("test_user_id");
+		identification.set = new JSONObject().put("test user property", "test value");
+		amplitude.identify(identification);
 		amplitude.setLogMode(AmplitudeLog.LogMode.DEBUG);
 		return "Amplitude Java SDK Demo: sending test event.";
 	}
